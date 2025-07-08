c7db900da4add6d576828623c9c69f9882475301
kaungkhantcoder <kaungkhantcoder@gmail.com>
2025-07-08 23:42:30 +0630

    🦭 first commit with errors

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/kaungkhantcoder/go-fiber-postgres/storage"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	Author		string		`json:"author"`
+	Title		string		`json:"title"`
+	Publisher	string		`json:"publisher"`
+}
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) CreateBook(context *fiber.Ctx) error {
+	book := Book{}
+
+	err := context.BodyParser(&book)
+
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+		return err
+	}
+
+	err := r.DB.Create(&book).Error
+
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not create book"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(
+		&fiber.Map{"message": "book has beed added"})
+	return nil
+}
+
+func (r *Repository) GetBooks(context *fiber.Ctx) error{
+	bookModels := &[]models.Books{}
+
+	err := r.DB.Find(bookModels).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get books"})
+			return err
+	}
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "books fetched successfully",
+		"data": bookModels,
+	})
+	return nil
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_books", r.CreateBook)
+	api.Delete("delete_book/:id", r.DeleteBook)
+	api.Get("/get_books/:id", r.GetBookByID)
+	api.Get("/books", r.GetBooks)
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &storage.Config{
+		Host: os.Getenv("DB_HOST"),
+		Port: os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User: os.Getenv("DB_USER"),
+		SSLMode: os.Getenv("DB_SSLMODE"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+
+	db, err := storage.NewConnection(config)
+
+	if err != nil {
+		log.Fatal("could not load the database")
+	}
+
+	r := Repository{
+		DB: db,
+	}
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8080")
+}
